b9e0a95c00743d61057e38b0cb03eb3d2d226e2a
Chay Williams <Chayvw@gmail.com>
2021-06-29 14:43:04 -0400

    created prompt to ask user if they would like to view all employess or view by role. created two funnctions to cnnect to mysql and carry out choice selected

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
-console.log("Hello World");
-
+const connection = require("./config/connection");
+const inquirer = require("inquirer");
 const logo = require("asciiart-logo");
 
-const connection = require("./config/connection");
 
-connection.query("select * from employee", (err, data) =>{
-    if(err) throw err;
-    console.log(data)
-});
+function init() {
+    const logoText = logo({ name: "Employee Manager" }).render();
+    console.log(logoText);
+    // inquirer.prompt takes an array as an argument
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "what would you like to do?",
+                choices: [
+                    {
+                        name: "View All Employees",
+                        value: "VIEW"
+                    },
+
+                    {
+                        name: "View All Roles",
+                        value: "VIEW_ROLES"
+                    },
+                ],
+            }
+        ])
+        .then((response) => {
+            console.log(response);
+            if(response.choice === "VIEW"){
+            orm.viewAll();
+            } else if (response.choice === "VIEW_ROLES"){
+                orm.viewRoles();
+            }
+        });
+
+}
+
+const orm = {
+    viewAll: () => {
+        connection.query("select * from employee", (err, allEmployees) => {
+            if (err) throw err;
+            console.table(allEmployees);
+            // run init again
+            // init();
+        });
+    },
+    viewRoles: ()=>{
+        connection.query("select * from role", (err, allRoles) => {
+            if (err) throw err;
+            console.table(allRoles);
+        });
+
+    }
+}
+
+
+
+init()
