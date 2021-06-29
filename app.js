58359b228b297ba7562d0231cd9e53fe44615d2c
Chay Williams <Chayvw@gmail.com>
2021-06-29 15:31:14 -0400

    debugged an error but had to change my view functions to es5 and set up a connection query where a user can view all deparmtents

--- app.js
+++ app.js
@@ -23,38 +23,77 @@ function init() {
                         name: "View All Roles",
                         value: "VIEW_ROLES"
                     },
+
+                    {
+                        name: "View All Departments",
+                        value: "VIEW_DEPARTMENTS"
+                    },
                 ],
             }
         ])
         .then((response) => {
             console.log(response);
-            if(response.choice === "VIEW"){
-            orm.viewAll();
-            } else if (response.choice === "VIEW_ROLES"){
+            if (response.choice === "VIEW") {
+                orm.viewAll();
+            } else if (response.choice === "VIEW_ROLES") {
                 orm.viewRoles();
+            } else if (response.choice === "VIEW_DEPARTMENTS") {
+                orm.viewDepartments();
+                //get a list of departments 
+                connection.query("select * from department", (err, allDepartments) => {
+                    console.table(allDepartments);
+                    // departmentChoice is a way for me to map through the department table and return back name and id in an array
+                    const departmentChoices = allDepartments.map((department) => {
+                        const newChoice = {
+                            name: department.name,
+                            value: department.id
+                        };
+                        return newChoice
+                    })
+                    inquirer
+                        .prompt([
+                            {
+                                type: "list",
+                                name: "choice",
+                                message: "what would you like to view?",
+                                choices: departmentChoices
+                            }
+                        ]).then((secondResponse) => {
+                            console.log(secondResponse);
+                        })
+
+                });
+
+                // display the list to the user 
+                // all the user to select value 
             }
         });
 
 }
 
 const orm = {
-    viewAll: () => {
-        connection.query("select * from employee", (err, allEmployees) => {
+    findAll: function (tableName) {
+        connection.query("select * from ??", [tableName], (err, allEmployees) => {
             if (err) throw err;
             console.table(allEmployees);
-            // run init again
-            // init();
         });
+
     },
-    viewRoles: ()=>{
-        connection.query("select * from role", (err, allRoles) => {
-            if (err) throw err;
-            console.table(allRoles);
-        });
 
-    }
-}
+    viewAll: function () {
+        this.findAll("employee");
+    },
 
+    viewRoles: function () {
+        this.findAll("role");
+
+    },
+
+    viewDepartments: function () {
+        this.findAll("department");
+
+    },
+};
 
 
 init()
